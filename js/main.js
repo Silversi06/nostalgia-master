268dd7ebd3f18870b607fb0c18331054065fffa2
GoEun <w2219@e-mirim.hs.kr>
2023-06-22 00:32:14 +0900

    page3 css 수정

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var quizData = [
+    {
+      question: "계절선택",
+      options: [
+        { image: "summer.png", isAnswer: true },
+        { image: "spring.png", isAnswer: false },
+        { image: "fall.png", isAnswer: false },
+        { image: "winter.png", isAnswer: false }
+      ]
+    },
+    {
+      question: "취향선택",
+      options: [
+        { image: "yellow_btn1.png", isAnswer: true },
+        { image: "yellow_btn2.png", isAnswer: false },
+        { image: "yellow_btn3.png", isAnswer: false },
+        { image: "yellow_btn4.png", isAnswer: false }
+      ]
+    },
+    {
+      question: "동물 선택",
+      options: [
+        { image: "토끼.png", isAnswer: true },
+        { image: "비글.png", isAnswer: false },
+        { image: "판다.png", isAnswer: false },
+        { image: "늑대.png", isAnswer: false }
+      ]
+    }
+  ];
